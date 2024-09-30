88c5a4900500add28f20c1c51f962df1b21f3c2e
CalistoDel <calistodelaguila@gmail.com>
2024-09-30 15:54:00 +0200

    Pendu 1ère version

---
+++ fonctions_TP3.py
@@ -0,0 +1,60 @@
+import random
+
+def mots():
+    fichier=open('motspendu.txt','r')
+    mots=fichier.readlines()
+    numero_mot=random.randint(0,len(mots)-1)
+    mot_pendu=mots[numero_mot]
+    mot_chercher=[mot_pendu[0]]
+    for n in range(len(mot_pendu)-2):
+        mot_chercher.append('_')
+    return mot_chercher, mot_pendu
+
+
+
+def affichage(mot_chercher,lettre,mot_pendu):
+    for i in range(len(mot_chercher)):
+        if mot_pendu[i]==lettre:
+            mot_chercher[i]=lettre
+    return mot_chercher
+
+
+
+def verification_lettre(mot_pendu,lettre):
+    Let=[]
+    for j in range(len(mot_pendu)):
+        if lettre==mot_pendu[j]:
+            Let.append(j)
+            return True, Let
+        else:
+            return False
+
+
+
+
+def jeu():
+    nbre_erreurs=0
+    mot_pendu,mot_chercher=mots()
+    while mot_chercher!=mot_pendu and nbre_erreurs<=8:
+        M=''
+        for j in range(len(mot_pendu)):
+            M=M+mot_chercher[j]
+            print('choisissez une lettre:')
+            lettre=input()
+            verif=verification_lettre(mot_pendu,lettre)
+            if verif==False:
+                nbre_erreurs = nbre_erreurs+1
+            else:
+                M=''
+                for i in range(len(mot_pendu)):
+                    if mot_pendu[i]==lettre:
+                        mot_chercher[i]=lettre
+                    M=M+mot_chercher[i]
+                print(M)
+    M=''
+    for i in range(len(mot_pendu)):
+        M=M+mot_pendu[i]
+                
+
+
+jeu()
