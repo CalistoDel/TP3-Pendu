0bde52129cfb1e5228b3e65221cfb2a7621115b0
CalistoDel <calistodelaguila@gmail.com>
2024-09-30 16:10:58 +0200

    Pendu

--- fonctions_TP3.py
+++ fonctions_TP3.py
@@ -6,24 +6,26 @@ def mots():
     numero_mot=random.randint(0,len(mots)-1)
     mot_pendu=mots[numero_mot]
     mot_chercher=[mot_pendu[0]]
+    motpe=[mot_pendu[0]]
     for n in range(len(mot_pendu)-2):
         mot_chercher.append('_')
-    return mot_chercher, mot_pendu
+        motpe.append(mot_pendu[n])
+    return mot_chercher, motpe
 
 
 
-def affichage(mot_chercher,lettre,mot_pendu):
+def affichage(mot_chercher,lettre,motpe):
     for i in range(len(mot_chercher)):
-        if mot_pendu[i]==lettre:
+        if motpe[i]==lettre:
             mot_chercher[i]=lettre
     return mot_chercher
 
 
 
-def verification_lettre(mot_pendu,lettre):
+def verification_lettre(motpe,lettre):
     Let=[]
-    for j in range(len(mot_pendu)):
-        if lettre==mot_pendu[j]:
+    for j in range(len(motpe)):
+        if lettre==motpe[j]:
             Let.append(j)
             return True, Let
         else:
@@ -34,27 +36,30 @@ def verification_lettre(mot_pendu,lettre):
 
 def jeu():
     nbre_erreurs=0
-    mot_pendu,mot_chercher=mots()
-    while mot_chercher!=mot_pendu and nbre_erreurs<=8:
+    motpe,mot_chercher=mots()
+    while mot_chercher!=motpe and nbre_erreurs<=8:
         M=''
-        for j in range(len(mot_pendu)):
+        for j in range(len(motpe)):
             M=M+mot_chercher[j]
+            print(M)
             print('choisissez une lettre:')
             lettre=input()
-            verif=verification_lettre(mot_pendu,lettre)
+            verif=verification_lettre(motpe,lettre)
             if verif==False:
                 nbre_erreurs = nbre_erreurs+1
             else:
                 M=''
-                for i in range(len(mot_pendu)):
-                    if mot_pendu[i]==lettre:
+                for i in range(len(motpe)):
+                    if motpe[i]==lettre:
                         mot_chercher[i]=lettre
                     M=M+mot_chercher[i]
                 print(M)
     M=''
-    for i in range(len(mot_pendu)):
-        M=M+mot_pendu[i]
+    for i in range(len(motpe)):
+        M=M+motpe[i]
                 
 
 
 jeu()
+
+
